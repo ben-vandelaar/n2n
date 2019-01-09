0f24e0b159e8f847e076be6d54dc95945a8a9009
ben-vandelaar <benvandelaar@gmail.com>
2019-01-10 11:41:29 +1300

    Chapter 7

--- main.js
+++ main.js
@@ -177,54 +177,172 @@ const quiz = [
 //View Object
 
 
-const view = {
-    score: document.querySelector('#score strong'),
-    question: document.getElementById('question'),
-    result: document.getElementById('result'),
-    info: document.getElementById('info'),
-    render(target, content, attributes) {
-        for (const key in attributes) {
-            target.setAttribute(key, attributes[key]);
-        }
-        target.innerHTML = content;
-    }
-};
+// const view = {
+//     score: document.querySelector('#score strong'),
+//     question: document.getElementById('question'),
+//     result: document.getElementById('result'),
+//     info: document.getElementById('info'),
+//     render(target, content, attributes) {
+//         for (const key in attributes) {
+//             target.setAttribute(key, attributes[key]);
+//         }
+//         target.innerHTML = content;
+//     }
+// };
 
 
 //Game Object
 
 
-const game = {
-    start(quiz) {
-        this.questions = [...quiz];
-        this.score = 0;
+// const game = {
+//     start(quiz) {
+//         this.questions = [...quiz];
+//         this.score = 0;
 
-        for (const question of this.questions) {
-            this.question = question;
-            this.ask();
-        }
-        this.gameOver();
-    },
-    ask() {
-        const question = `What is ${this.question.name}'s real name ?`;
-        view.render(view.question, question);
-        const response = prompt(question);
-        this.check(response);
-    },
-    check(response) {
-        const answer = this.question.realName;
-        if (response === answer) {
-            view.render(view.result, 'Correct!', { 'class': 'correct' });
-            alert('Correct!');
-            this.score++;
-            view.render(view.score, this.score);
-        } else {
-            view.render(view.result, `Wrong! The correct answer was ${answer}`);
-        }
-    },
-    gameOver() {
-        view.render(view.info, `Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`)
-    }
+//         for (const question of this.questions) {
+//             this.question = question;
+//             this.ask();
+//         }
+//         this.gameOver();
+//     },
+//     ask() {
+//         const question = `What is ${this.question.name}'s real name ?`;
+//         view.render(view.question, question);
+//         const response = prompt(question);
+//         this.check(response);
+//     },
+//     check(response) {
+//         const answer = this.question.realName;
+//         if (response === answer) {
+//             view.render(view.result, 'Correct!', { 'class': 'correct' });
+//             alert('Correct!');
+//             this.score++;
+//             view.render(view.score, this.score);
+//         } else {
+//             view.render(view.result, `Wrong! The correct answer was ${answer}`);
+//         }
+//     },
+//     gameOver() {
+//         view.render(view.info, `Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`)
+//     }
+// }
+
+// game.start(quiz);
+
+
+//Chapter 7
+
+function doSomething() {
+    console.log('Something Happened!');
 }
 
-game.start(quiz);
+// addEventListener('click', doSomething2)
+
+// function doSomething2(event) {
+//     console.log(`screen: (${event.screenX}, ${event.screenY}),
+//     page: (${event.pageX}, ${event.pageY}),
+//     client:(${event.screenX},${event.screenY})`)
+// }
+
+// const dblclickParagraph = document.getElementById('dblclick');
+// dblclickParagraph.addEventListener('dblclick', highlight)
+
+// function highlight(event) {
+//     event.target.classList.toggle('highlight');
+// }
+
+// const mouseParagraph = document.getElementById('mouse');
+// mouseParagraph.addEventListener('mouseover', highlight);
+// mouseParagraph.addEventListener('mouseout', highlight);
+
+// addEventListener('keydown', highlight);
+
+// addEventListener('keydown', (event) => console.log(`You pressed the ${event.key} character`));
+
+// addEventListener('keydown', (event) => {
+//     if (event.key === 'c' && event.ctrlKey) {
+//         console.log('Action Cancled!');
+//     }
+// })
+
+// addEventListener('touched', () => console.log('Touch stopped'));
+
+
+// ulElement = document.getElementById('list');
+// liElement = document.querySelector('#list li');
+
+// ulElement.addEventListener('click', (event) =>
+//     console.log('Clicked on ul'), true);
+
+// liElement.addEventListener('click', (event) => {
+//     console.log('Clicked on li'),
+//         event.stopPropagation();
+// }, false);
+
+// const view = {
+//     start: document.getElementById('start'),
+//     score: document.querySelector('#score strong'),
+//     question: document.getElementById('question'),
+//     result: document.getElementById('result'),
+//     info: document.getElementById('info'),
+
+//     show(element) {
+//         element.style.display = "block";
+//     },
+//     hide(element) {
+//         element.style.display = 'none';
+//     },
+
+//     render(target, content, attributes) {
+//         for (const key in attributes) {
+//             target.setAttribute(key, attributes[key]);
+//         }
+//         target.innerHTML = content;
+//     }
+// };
+
+//Game Object
+
+
+// const game = {
+//     start(quiz) {
+//         view.hide(view.start);
+//         this.questions = [...quiz];
+//         this.score = 0;
+
+//         for (const question of this.questions) {
+//             this.question = question;
+//             this.ask();
+//         }
+//         this.gameOver();
+//     },
+//     ask() {
+//         const question = `What is ${this.question.name}'s real name ?`;
+//         view.render(view.question, question);
+//         const response = prompt(question);
+//         this.check(response);
+//     },
+//     check(response) {
+//         const answer = this.question.realName;
+//         if (response === answer) {
+//             view.render(view.result, 'Correct!', { 'class': 'correct' });
+//             alert('Correct!');
+//             this.score++;
+//             view.render(view.score, this.score);
+//         } else {
+//             view.render(view.result, `Wrong! The correct answer was ${answer}`);
+//         }
+//     },
+//     gameOver() {
+//         view.show(view.start);
+//         view.render(view.info, `Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`)
+//     }
+// }
+
+// view.start.addEventListener('click', () =>
+//     game.start(quiz), false);
+
+
+
+//Chapter 8
+
